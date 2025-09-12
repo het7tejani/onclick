3bb176c29d6bc964417ba4957f0914addde69d5a
het7tejani <hettj149@gmail.com>
2025-09-12 16:43:47 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -175,11 +175,13 @@ app.post('/api/analyze-idea', async (req, res) => {
 
 // ===== Serve static assets in production =====
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
   });
 }
+
 
 // ===== Start Server =====
 app.listen(PORT, () => {
